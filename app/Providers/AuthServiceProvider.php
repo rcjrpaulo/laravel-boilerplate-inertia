20abbee8811f54fa043ef4475699ceb7baf97e55
Paulo Cardoso <paulo@agily.com.br>
2021-06-30 09:52:41 -0300

    gate permissions

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Permissao;
+use App\Models\Permission;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -23,8 +26,20 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies();
+        //        $this->registerPolicies();
 
-        //
+        if (Schema::hasTable('permissions')) {
+            $permissions = Permission::get(['name'])->pluck('name')->toArray();
+
+            foreach ($permissions as $permission) {
+                Gate::define($permission, function ($user) use ($permission) {
+                    if (empty($user->role->permissions->pluck('name')->toArray())) {
+                        return false;
+                    }
+
+                    return in_array($permission, $user->role->permissions->pluck('name')->toArray());
+                });
+            }
+        }
     }
 }
