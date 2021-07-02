50af37b3c93bd984365793e0c74f5009f050cc9d
Paulo Cardoso <paulo@agily.com.br>
2021-07-02 12:50:54 -0300

    estrutura de feature tests feita

---
+++ tests/Feature/Web/UserControllerTest.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Tests\Feature\Web;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class UserControllerTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /*
+     * CREATE
+     * */
+
+    /** @test */
+    public function criar_usuario()
+    {
+        $this->assertDatabaseCount('users', 2);
+
+        $dados = [
+            'name' => 'teste',
+            'email' => 'teste@teste.com',
+            'role_id' => Role::first()->id,
+            'photo' => null,
+            'password' => 'teste',
+        ];
+
+        $this->post(route('users.store'), $dados)
+            ->assertStatus(302);
+
+        $this->assertDatabaseHas('users', ['name' => 'teste']);
+        $this->assertDatabaseCount('users', 3);
+    }
+
+    /*
+     * READ - List
+     * */
+
+    /** @test */
+    public function listar_usuarios()
+    {
+        $usuario = User::factory()->create();
+        User::factory()->count(2)->create();
+
+        $response = $this->get(route('users.index'))
+            ->assertStatus(200);
+    }
+
+    /*
+     * READ - Show
+     * */
+
+    /** @test */
+    public function mostrar_usuario()
+    {
+        $usuario = User::factory()->create();
+
+        $this->get(route('users.show', $usuario))
+            ->assertStatus(200);
+    }
+
+    /*
+     * UPDATE
+     * */
+
+    /** @test */
+    public function atualizar_usuario()
+    {
+        $usuario = User::factory()->create(['name' => 'old name']);
+
+        $this->assertDatabaseHas('users', ['name' => 'old name']);
+
+        $dados = [
+            'name' => 'new name',
+            'email' => 'mail@mail.com',
+            'role_id' => '1',
+            'photo' => null,
+        ];
+
+        $this->put(route('users.update', $usuario), $dados)
+            ->assertStatus(302);
+
+        $this->assertDatabaseHas('users', ['name' => 'new name']);
+        $this->assertDatabaseMissing('users', ['name' => 'old name']);
+    }
+
+    /*
+     * DELETE
+     * */
+
+    /** @test */
+    public function deletar_usuario()
+    {
+        $usuario = User::factory()->create();
+
+        $this->assertDatabaseHas('users', ['name' => $usuario->name]);
+
+        $this->delete(route('users.destroy', $usuario))
+            ->assertStatus(302);
+
+        $this->assertDatabaseMissing('users', ['name' => $usuario->name]);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,39 @@
 
 namespace Tests;
 
+use App\Models\Permission;
+use App\Models\User;
+use Faker\Factory;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected $faker;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->faker = Factory::create();
+
+        // Run the DatabaseSeeder...
+        $this->seed();
+
+        // user admin autenticado
+        $this->actingAs(User::where('email', 'admin@admin.com')->first());
+
+        //gates
+        if (Schema::hasTable('permissions')) {
+            $permissions = Permission::get(['name'])->pluck('name')->toArray();
+
+            foreach ($permissions as $permission) {
+                Gate::define($permission, function ($user) use ($permission) {
+                    return true;
+                });
+            }
+        }
+    }
 }
